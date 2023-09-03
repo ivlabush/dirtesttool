00a95c84a2816302de77e9483362f741cd71f177
Ivan L <ivlabush@gmail.com>
2023-09-03 19:30:25 +0300

    Day 4: fix task results representation

--- src/main/java/com/example/dirtestservice/dto/TaskResultDto.java
+++ src/main/java/com/example/dirtestservice/dto/TaskResultDto.java
@@ -1,12 +1,11 @@
 package com.example.dirtestservice.dto;
 
 import lombok.Data;
-import org.springframework.http.HttpStatus;
 
 @Data
 public class TaskResultDto {
 
     private String id;
     private String url;
-    private HttpStatus status;
+    private int status;
 }
